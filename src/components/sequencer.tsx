fb862d7382a426840b1e51d4ce931528adc747c1
douglasdriving <douglasdriving@gmail.com>
2023-07-06 22:38:44 +0200

    created sequencer component

--- src/components/sequencer.tsx
+++ src/components/sequencer.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Sequencer from './components/sequencer';
 
 function App() {
   return (
     <div className="App">
-      <Sequencer />
+      <p>
+        Sequencer
+      </p>
     </div>
   );
 }
