2e59404cbd8168c1764d29c03905f911da68aab5
douglasdriving <douglasdriving@gmail.com>
2023-08-10 00:14:51 +0200

    Update reflectionHistory.tsx

--- src/components/reflectionHistory/reflectionHistory.tsx
+++ src/components/reflectionHistory/reflectionHistory.tsx
@@ -1,6 +1,6 @@
 import ReflectionI from "../../interfaces/reflection";
 import { useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ReflectionHistory = () => {
 
@@ -17,38 +17,41 @@ const ReflectionHistory = () => {
   };
 
   return (
-    <div>
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="flex flex-col">
 
-      <h1>Reflections</h1>
+        <h1 className="text-3xl font-bold mb-1">Reflections</h1>
 
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
+        <div className="flex flex-row">
 
-        <div>
-          {reflections.map((reflection, index) => (
-            <div key={index}>
-              <button onClick={() => handleClick(index)}
-                style={{
-                  backgroundColor: reflectionIndex === index ? 'gray' : 'white',
-                  color: reflectionIndex === index ? 'white' : 'black',
-                }}>
-                {reflection.date}
-              </button>
-            </div>
-          ))}
+          <div>
+            {reflections.map((reflection, index) => (
+              <div key={index}>
+                <button onClick={() => handleClick(index)}
+                  style={{
+                    backgroundColor: reflectionIndex === index ? 'gray' : 'white',
+                    color: reflectionIndex === index ? 'white' : 'black',
+                  }}>
+                  {reflection.date}
+                </button>
+              </div>
+            ))}
+          </div>
+
+          {reflectionIndex !== null &&
+            <p 
+            className="p-2 m-2 text-left bg-gray-500 text-white w-96 mt-0">
+              {reflections[reflectionIndex].text.split('\n').map((line, index) => (
+                <span key={index}>{line}<br /></span>
+              ))}
+            </p>
+          }
+
         </div>
 
-        {reflectionIndex !== null &&
-          <p style={{ border: '2px solid black', padding: 10, margin: 10, textAlign: 'left' }}>
-            {reflections[reflectionIndex].text.split('\n').map((line, index) => (
-              <span key={index}>{line}<br /></span>
-            ))}
-          </p>
-        }
+        <button onClick={() => navigate('/start_sequence')}>Home</button>
 
       </div>
-
-      <button onClick={() => navigate('/start_sequence')}>Home</button>
-
     </div>
   );
 }
