94d6b5b48fa3379598b327b8afe456b7e58dd723
douglasdriving <douglasdriving@gmail.com>
2023-09-01 16:22:35 -0600

    started implementing the login UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import './App.css';
 import Root from './components/root';
 
+//for supabase
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+//maybe env var? check how it is done for github pages
+const supabase = createClient(
+  'https://aiqjywvacfonoaxcnzog.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcWp5d3ZhY2Zvbm9heGNuem9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM2MDU4MzcsImV4cCI6MjAwOTE4MTgzN30.j2j1R45mmxmi9XxuzG4NbRbcHsGdsNPyGoJfj94LKuE'
+);
+
 function App() {
   return (
     <Root />
